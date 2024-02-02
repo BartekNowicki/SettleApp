dcb404f0d41353237748afea96b4dba047db6afb
Bartosz Nowicki <bartosznowickihq@gmail.com>
2024-02-02 21:09:50 +0100

    Rename the backend app

--- src/main/java/com/application/settleApp/SettleAppApplication_BE.java
+++ src/main/java/com/application/settleApp/SettleAppApplication_BE.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SettleAppApplication {
+public class SettleAppApplication_BE {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SettleAppApplication.class, args);
+		SpringApplication.run(SettleAppApplication_BE.class, args);
 		System.out.println("YOOOOOOOOOOOOO");
 	}
 
