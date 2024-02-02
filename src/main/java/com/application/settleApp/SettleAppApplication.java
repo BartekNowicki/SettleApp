964b269f98794b5e424f24d51e136165bfcb9ae9
Bartosz Nowicki <bartosznowickihq@gmail.com>
2024-02-02 20:51:54 +0100

    Initial commit

---
+++ src/main/java/com/application/settleApp/SettleAppApplication.java
@@ -0,0 +1,14 @@
+package com.application.settleApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettleAppApplication.class, args);
+		System.out.println("YOOOOOOOOOOOOO");
+	}
+
+}
---
+++ src/test/java/com/application/settleApp/SettleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.settleApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
