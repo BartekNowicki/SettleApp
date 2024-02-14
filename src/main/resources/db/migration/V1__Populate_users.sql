5240cb5a53bee928e89088b35d21e26fcc11d21b
Bartosz Nowicki <bartosznowickihq@gmail.com>
2024-02-14 10:29:49 +0100

    Enable flyway migrations

---
+++ src/main/resources/db/migration/V1__Populate_users.sql
@@ -0,0 +1,6 @@
+INSERT INTO user_table (fname, lname) VALUES ('InitialUser1_fname', 'InitialUser1_lname');
+INSERT INTO user_table (fname, lname) VALUES ('InitialUser2_fname', 'InitialUser2_lname');
+INSERT INTO user_table (fname, lname) VALUES ('InitialUser3_fname', 'InitialUser3_lname');
+INSERT INTO user_table (fname, lname) VALUES ('InitialUser4_fname', 'InitialUser4_lname');
+INSERT INTO user_table (fname, lname) VALUES ('InitialUser5_fname', 'InitialUser5_lname');
+INSERT INTO user_table (fname, lname) VALUES ('InitialUser6_fname', 'InitialUser6_lname');
