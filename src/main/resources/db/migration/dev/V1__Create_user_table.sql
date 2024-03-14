dcc060eccd4852b7da713216ee648608fa1b3fbf
Bartosz Nowicki <bartosznowickihq@gmail.com>
2024-03-14 12:26:25 +0100

    Change migrations which create entities to include creation and modification dates set to default values

--- src/main/resources/db/migration/dev/V1__Create_user_table.sql
+++ src/main/resources/db/migration/dev/V1__Create_user_table.sql
@@ -1,8 +1,10 @@
 CREATE TABLE IF NOT EXISTS user
 (
-    user_id  BIGINT AUTO_INCREMENT PRIMARY KEY,
-    fname    VARCHAR(255)        NOT NULL,
-    lname    VARCHAR(255)        NOT NULL,
-    email    VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255)        NOT NULL
+    user_id           BIGINT AUTO_INCREMENT PRIMARY KEY,
+    fname             VARCHAR(255)        NOT NULL,
+    lname             VARCHAR(255)        NOT NULL,
+    email             VARCHAR(255) UNIQUE NOT NULL,
+    password          VARCHAR(255)        NOT NULL,
+    creation_date     TIMESTAMP DEFAULT '2024-01-01 00:00:00' NOT NULL,
+    modification_date TIMESTAMP DEFAULT '2024-01-01 00:00:00' NOT NULL
 );
